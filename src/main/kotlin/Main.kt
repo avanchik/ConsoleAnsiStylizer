707443728f6ba123ba968f5117e6efa6082cfd4a
Ruslan Onishchuk <ruslan.onishchuk.90@gmail.com>
2023-12-31 09:51:56 +0200

    add README.MD

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,18 +3,15 @@ import ConsoleAnsiStylize.Styles
 import ConsoleAnsiStylize.stylizeText
 
 fun main() {
-    // Демонстрация разных стилей текста
     println("\nРазные стили текста:")
     Styles.entries.forEach { print(stylizeText(it.name, styles = arrayOf(it)) + " ") }
 
-    // Демонстрация разных цветов текста
     println("\nРазные цвета текста:")
     Colors.entries.forEach {
         print(stylizeText(it.name, fgColor = it) + " ")
     }
     println()
 
-    // Демонстрация разных цветов фона
     println("\nРазные цвета фона:")
     Colors.entries.forEach {
         print(stylizeText("${it.name} фон", bgColor = it) + " ")
